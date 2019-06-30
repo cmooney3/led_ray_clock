e66b3d450d879db1435a5a281979866bf9b1b359
Charlie Mooney <cmooney3@gmail.com>
2019-06-29 20:27:06 -0700

    Get the RTC working
    This required some PCB redesigns
    
    I couldn't get the RTC to work on other pins than the defaults (4, 5) so I had to
    move the LED pin over to pin 13 since it used to be on 5.  This was done by hacking
    the PCB, but could be made permanent in a future board if I end up making more.
    
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- led_ray_clock.ino
+++ led_ray_clock.ino
@@ -5,14 +5,12 @@
 Scheduler scheduler;
 
 #include <Wire.h>
-#include <RTClib.h>
-RTC_DS3231 rtc;
-static constexpr uint8_t kRtcSdaPin = 0;
-static constexpr uint8_t kRtcSclPin = 2;
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
 
 // Pin assignments
 static constexpr uint8_t kButtonPin = 0;
-static constexpr uint8_t kLEDPin = 5;
+static constexpr uint8_t kLEDPin = 13;
 
 // LED details
 constexpr uint16_t kNumLEDs = 36; // How Many LEDs are connected
@@ -31,34 +29,41 @@ void setupFastLED() {
 
 void setupRTC() {
   // Setup the real time clock (RTC)
+  Rtc.Begin();
 
-  // The RTC works over I2C so we set up the Arduino Wire library on the I2c pins we're using.
-  Wire.begin(kRtcSdaPin, kRtcSclPin);  // Use this instead of rtc.begin() so I can set the pins
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, ReSetting the time to 12:00!");
-    rtc.adjust(DateTime(2019, 1, 1, 0, 0, 0));
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  if (!Rtc.IsDateTimeValid()) {
+    Serial.println("RTC lost confidence in the DateTime!");
+    Rtc.SetDateTime(compiled);
   }
 }
 
-DateTime now(2019, 1, 1, 0, 0, 0);
+RtcDateTime now(2019, 1, 1, 11, 20, 40);
 void updateTime() {
-  now = now + TimeSpan(1); // Advance 1 second
+  Serial.println("Querying RTC for time.");
+  now = Rtc.GetDateTime();
+  Serial.print(now.Hour());
+  Serial.print(":");
+  Serial.print(now.Minute());
+  Serial.print(":");
+  Serial.println(now.Second());
 }
 Task taskUpdateTime(TASK_SECOND, TASK_FOREVER, &updateTime);
 
 void updateDisplay() {
+  Serial.println("Displaying time.");
+
   // Update the display with the current time
   fill_solid(leds, kNumLEDs, CRGB::Black);
 
-  unsigned int second_led = now.second() * kNumLEDs / 60;
+  unsigned int second_led = now.Second() * kNumLEDs / 60;
   leds[kNumLEDs - second_led - 1] = CRGB::Red;
 
-  unsigned int minute_led = now.minute() * kNumLEDs / 60;
+  unsigned int minute_led = now.Minute() * kNumLEDs / 60;
   leds[kNumLEDs - minute_led - 1] = CRGB::Green;
 
-  unsigned int hour_led = now.hour() * kNumLEDs / 12;
-  hour_led += (kNumLEDs * now.minute()) / (12 * 60); // Advance within the hour based on the minute
+  unsigned int hour_led = (now.Hour() % 12) * kNumLEDs / 12;
+  hour_led += (kNumLEDs * now.Minute()) / (12 * 60); // Advance within the hour based on the minute
   leds[kNumLEDs - hour_led - 1] = CRGB::Blue;
 
   FastLED.show();
@@ -66,22 +71,22 @@ void updateDisplay() {
 Task taskUpdateDisplay(TASK_SECOND, TASK_FOREVER, &updateDisplay);
 
 void fastForwardTime() {
-  static int pressed_duration = 0;
-
-  // If the button is pressed down advance time by increasingly larger chunks
-  if (!digitalRead(kButtonPin)) {
-    pressed_duration++;
-    if (pressed_duration < 60) {
-      now = now + TimeSpan(1);
-    } else if (pressed_duration < 7 * 60 + 60) {
-      now = now + TimeSpan(7);
-    } else {
-      now = now + TimeSpan(70);
-    }
-    updateDisplay();
-  } else {
-    pressed_duration = 0;
-  }
+//  static int pressed_duration = 0;
+//
+//  // If the button is pressed down advance time by increasingly larger chunks
+//  if (!digitalRead(kButtonPin)) {
+//    pressed_duration++;
+//    if (pressed_duration < 60) {
+//      now = now + TimeSpan(1);
+//    } else if (pressed_duration < 7 * 60 + 60) {
+//      now = now + TimeSpan(7);
+//    } else {
+//      now = now + TimeSpan(70);
+//    }
+//    updateDisplay();
+//  } else {
+//    pressed_duration = 0;
+//  }
 }
 Task taskFastForwardTime(TASK_SECOND / 100, TASK_FOREVER, &fastForwardTime);
 
@@ -95,6 +100,10 @@ void setup() {
   // Set up FastLED to control the actual LEDs.  This makes them enabled but
   // turns them all off, so it won't start as a big flash of random colors.
   setupFastLED();
+
+  // Set up the real time clock.  This mostly means configuring i2c and programming
+  // in a time if the RTC has lost power at some point.
+  setupRTC();
 
   // Set up a task to update the time
   scheduler.addTask(taskUpdateTime);
