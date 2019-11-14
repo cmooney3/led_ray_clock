36911ad7e060b3d7c2c4d9020403a6485fbd8ea3
Charlie Mooney <cmooney3@gmail.com>
2019-11-13 20:08:12 -0800

    Cleaning up the ButtonHandler inferface a bit more
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- ButtonHandler.h
+++ ButtonHandler.h
@@ -1,9 +1,10 @@
 // Constants to abstract away which boolean value is pressed/released
 constexpr bool kButtonPressed = false;
-constexpr bool kButtonReleased = false;
+constexpr bool kButtonReleased = true;
 
 // Which gpio's are connected to the buttons and some memory to store their values
 constexpr uint8_t kNumButtons = 4;
+constexpr uint8_t kNoPinSetupYet = 0xFF;
 constexpr uint8_t kButton1Pin = 28;
 constexpr uint8_t kButton2Pin = 29;
 constexpr uint8_t kButton3Pin = 30;
@@ -13,27 +14,34 @@ class ButtonHandler {
 public:
   void setup() {
     _buttons[0].setup(kButton1Pin);
-//    _buttons[1].setup(kButton2Pin);
-//    _buttons[2].setup(kButton3Pin);
-//    _buttons[3].setup(kButton4Pin);
+    _buttons[1].setup(kButton2Pin);
+    _buttons[2].setup(kButton3Pin);
+    _buttons[3].setup(kButton4Pin);
   }
 
   void checkStates() {
-    _buttons[0].update();
+    for (uint8_t i = 0; i < kNumButtons; i++) {
+      _buttons[i].checkState();
+    }
   }
 
 private:
   class Button {
   public:
-    Button() : _pin(0), _state(kButtonReleased) {}
+    Button() : _pin(kNoPinSetupYet), _state(kButtonReleased) {}
 
     void setup(uint8_t pin) {
       _pin = pin;
       pinMode(_pin, INPUT_PULLUP);
+      _state = getState();
     }
 
-    void update() {
-      bool newState = digitalRead(_pin);
+    void checkState() {
+      if (_pin == kNoPinSetupYet) {
+        return;
+      }
+
+      bool newState = getState();
       if (newState != _state) {
         if (newState == kButtonPressed) {
           Serial.println(F("Button pressed"));
@@ -41,10 +49,14 @@ private:
           Serial.println(F("Button released"));
         }
       }
-    _state = newState;
+      _state = newState;
     }
 
   private:
+    bool getState() const {
+      return digitalRead(_pin);
+    }
+
     uint8_t _pin;
     bool _state;
   };
